4aa17d91fcd619c658491c46d46e9022bcf8839a
kaduceusxx <kaduceusx@gmail.com>
2022-01-09 17:33:19 +0100

    Continuando con emits y ON sockets

--- models/server.js
+++ models/server.js
@@ -5,14 +5,19 @@ const cors = require('cors');
 class Server{
 
    constructor(){
-      this.app = express();
-      this.port = process.env.PORT;
+      this.app    = express();
+      this.port   = process.env.PORT;
+      this.server = require('http').createServer(this.app);
+      this.io     = require('socket.io')(this.server);
 
       //middlewares
       this.middlewares();
 
       //Rutas
       this.routes();
+
+      //Sockets
+      this.sockets();
    }
 
    middlewares(){
@@ -34,8 +39,22 @@ class Server{
       // this.app.use('/api/uploads', require('../routes/uploads'));
    }
 
+   sockets(){
+      this.io.on('connection', socket => {
+         socket.on('disconnect', ()=>{
+            console.log('Cliente desconectado', socket.id);
+         });
+         
+         socket.on('enviarmensaje', (payload)=>{
+            // console.log(payload);
+            this.io.emit('enviarmensaje', payload);
+         });
+      });
+   }
+
+
    listen(){
-      this.app.listen(this.port, ()=>{
+      this.server.listen(this.port, ()=>{
          console.log('Servidor corriendo en el puerto', this.port);
       });
    }
--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,2 +1,38 @@
 
-console.log("hoalsd");
+//Referencias del htmll
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+const txtMensaje = document.querySelector('#txtMensaje');
+const btnEnviar = document.querySelector('#btnEnviar');
+
+const socketCliente = io();
+
+socketCliente.on('connect', ()=>{
+   // console.log('connectadoo');
+   lblOffline.style.display = 'none';
+   lblOnline.style.display = '';
+});
+
+socketCliente.on('disconnect', ()=>{
+   console.log('desconnnectado');
+   lblOffline.style.display = '';
+   lblOnline.style.display = 'none';
+});
+
+
+socketCliente.on('enviarmensaje', (payload)=>{
+   console.log(payload);
+})
+
+btnEnviar.addEventListener('click', ()=>{
+
+   const mensaje = txtMensaje.value;
+   const payload = {
+      mensaje,
+      id: '1234',
+      fecha: new Date().getTime()
+   };
+   
+   socketCliente.emit('enviarmensaje', payload);
+   
+});
